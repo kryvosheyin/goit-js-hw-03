b100beaba152c57e08a368594f4c868b2ff5f8de
kryvosheyin <alex.kryvosheyin@gmail.com>
2024-04-15 12:15:48 +0100

    task_02 completed

---
+++ js/task-2.js
@@ -0,0 +1,26 @@
+const makeArray = (firstArray, secondArray, maxlength) => {
+    if (
+        !Array.isArray(firstArray) ||
+        !Array.isArray(secondArray) ||
+        typeof maxlength !== "number"
+    ) {
+        return "Invalid input";
+    }
+
+    if (maxlength < 0) {
+        return "Maximum length can not be less than 0";
+    }
+
+    const newArray = [...firstArray, ...secondArray];
+    if (newArray.length > maxlength) {
+        return newArray.slice(0, maxlength);
+    }
+    return newArray;
+};
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
